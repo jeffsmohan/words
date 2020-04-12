9650c281762d12875baa438eff2314fcace4ecd9
Jeff Mohan <jeffsmohan@gmail.com>
2020-04-12 17:44:57 +0800

    Add basic testing framework

---
+++ tests/corpora/twitter/test_twitter_tools.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestTwitterTools(unittest.TestCase):
+    """Tests for the Twitter corpus tooling."""
+
+    def test_passes(self):
+        """Test fake passing test."""
+        self.assertEqual(1 + 1, 2)
+
+    def test_fails(self):
+        """Test fake failing test."""
+        self.assertEqual(1 + 1, 1)
